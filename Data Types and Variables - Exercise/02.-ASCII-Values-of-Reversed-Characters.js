392bbfd90851ecff7a0300b24bd5bb9ed1789bb3
Vladimir Ilinkin <vladimirilinkin@Vladimirs-MacBook-Air.local>
2025-01-12 21:59:55 +0200

    feat: Add ASCII values conversion with character reversal

---
+++ Data Types and Variables - Exercise/02.-ASCII-Values-of-Reversed-Characters.js
@@ -0,0 +1,7 @@
+function aSCIIValuesReversedCharacters(param1, param2, param3) {
+    let lineOne = `` + param3 + param2 + param1;
+    console.log(lineOne);
+    console.log(
+      `${param3.charCodeAt()} ${param2.charCodeAt()} ${param1.charCodeAt()}`
+    );
+  }
